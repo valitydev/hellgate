62505508762ff5de079f6e99979043510f7756d8
kehitt <kehitt@users.noreply.github.com>
2022-04-20 15:49:08 +0300

    purge legacy marshalling

--- apps/hellgate/src/hg_cash.erl
+++ apps/hellgate/src/hg_cash.erl
@@ -6,8 +6,6 @@
 
 -export([add/2]).
 -export([sub/2]).
--export([marshal/1]).
--export([unmarshal/1]).
 
 -type cash() :: dmsl_domain_thrift:'Cash'().
 
@@ -24,27 +22,3 @@ sub(?cash(Amount1, Curr), ?cash(Amount2, Curr)) ->
     ?cash(Amount1 - Amount2, Curr);
 sub(_, _) ->
     error(badarg).
-
-%% Marshalling
-
--spec marshal(cash()) -> hg_msgpack_marshalling:value().
-marshal(Cash) ->
-    marshal(cash, Cash).
-
-marshal(cash, ?cash(Amount, SymbolicCode)) ->
-    [2, [Amount, SymbolicCode]];
-marshal(cash, undefined) ->
-    undefined.
-
-%% Unmarshalling
-
--spec unmarshal(hg_msgpack_marshalling:value()) -> cash().
-unmarshal(Cash) ->
-    unmarshal(cash, Cash).
-
-unmarshal(cash, [2, [Amount, SymbolicCode]]) ->
-    ?cash(Amount, SymbolicCode);
-unmarshal(cash, [1, {'domain_Cash', Amount, {'domain_CurrencyRef', SymbolicCode}}]) ->
-    ?cash(Amount, SymbolicCode);
-unmarshal(cash, undefined) ->
-    undefined.
