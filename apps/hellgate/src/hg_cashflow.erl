44d60fcfca2b80229b8fc845a0c008676165fcf6
Артем <WWW_cool@inbox.ru>
2022-04-26 19:55:41 +0300

    updated little test

--- apps/hellgate/src/hg_cashflow.erl
+++ apps/hellgate/src/hg_cashflow.erl
@@ -250,6 +250,22 @@ modify_remainder(#domain_FinalCashFlowAccount{account_type = AccountType}, ?cash
 
 compute_volume_test() ->
     Cash = ?cash(100, ?currency(<<"RUB">>)),
-    ?assertEqual(Cash, compute_volume(?fixed(Cash), #{})).
+    ?assertEqual(Cash, compute_volume(?fixed(Cash), #{})),
+    ?assertEqual(
+        ?cash(1, ?currency(<<"RUB">>)),
+        compute_volume(?share(1, 100, operation_amount, undefined), #{operation_amount => Cash})
+    ),
+    ?assertEqual(
+        Cash,
+        compute_volume(?product(min_of, [?fixed(Cash), ?fixed(?cash(200, ?currency(<<"RUB">>)))]), #{})
+    ),
+    ?assertEqual(
+        Cash,
+        compute_volume(?product(max_of, [?fixed(Cash), ?fixed(?cash(50, ?currency(<<"RUB">>)))]), #{})
+    ),
+    ?assertEqual(
+        ?cash(200, ?currency(<<"RUB">>)),
+        compute_volume(?product(sum_of, [?fixed(Cash), ?fixed(Cash)]), #{})
+    ).
 
 -endif.
