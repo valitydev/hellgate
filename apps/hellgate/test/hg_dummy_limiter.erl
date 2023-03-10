bfb87d928277da7faa1bba65246a84f7e5b58d65
Aleksey Kashapov <nanodirijabl@gmail.com>
2023-03-10 15:42:15 +0300

    Removes obsolete limiter configurator client call

--- apps/hellgate/test/hg_dummy_limiter.erl
+++ apps/hellgate/test/hg_dummy_limiter.erl
@@ -7,8 +7,6 @@
 -export([hold/3]).
 -export([commit/3]).
 
--export([get_config/2]).
-
 -type client() :: woody_context:ctx().
 
 -type limit_id() :: limproto_limiter_thrift:'LimitID'().
@@ -34,10 +32,6 @@ hold(LimitChange, Context, Client) ->
 commit(LimitChange, Context, Client) ->
     call('Commit', {LimitChange, clock(), Context}, Client).
 
--spec get_config(limit_id(), client()) -> woody:result() | no_return().
-get_config(LimitConfigID, Client) ->
-    call_configurator('Get', {LimitConfigID}, Client).
-
 %%% Internal functions
 
 -spec call(atom(), tuple(), client()) -> woody:result() | no_return().
@@ -45,18 +39,6 @@ call(Function, Args, Client) ->
     Call = {{limproto_limiter_thrift, 'Limiter'}, Function, Args},
     Opts = #{
         url => <<"http://limiter:8022/v1/limiter">>,
-        event_handler => scoper_woody_event_handler,
-        transport_opts => #{
-            max_connections => 10000
-        }
-    },
-    woody_client:call(Call, Opts, Client).
-
--spec call_configurator(atom(), tuple(), client()) -> woody:result() | no_return().
-call_configurator(Function, Args, Client) ->
-    Call = {{limproto_configurator_thrift, 'Configurator'}, Function, Args},
-    Opts = #{
-        url => <<"http://limiter:8022/v1/configurator">>,
         event_handler => scoper_woody_event_handler,
         transport_opts => #{
             max_connections => 10000
