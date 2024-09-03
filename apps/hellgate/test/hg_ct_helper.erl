d514b02fca43e038762bacb49b94cc2c3b11f4d8
Aleksey Kashapov <nanodirijabl@gmail.com>
2024-09-03 16:08:08 +0300

    IMP-278: Reverts damsel w/ legacy payouts supports

--- apps/hellgate/test/hg_ct_helper.erl
+++ apps/hellgate/test/hg_ct_helper.erl
@@ -425,11 +425,13 @@ create_shop_(
 ) ->
     ShopID = hg_utils:unique_id(),
     ContractID = hg_utils:unique_id(),
+    PayoutToolID = hg_utils:unique_id(),
 
-    ShopParams = make_shop_params(Category, ContractID),
+    ShopParams = make_shop_params(Category, ContractID, PayoutToolID),
     ShopAccountParams = #payproc_ShopAccountParams{currency = ?cur(Currency)},
 
     ContractParams = make_contract_params(TemplateRef, PaymentInstRef),
+    PayoutToolParams = make_payout_tool_params(),
 
     TurnoverLimits1 = genlib:define(TurnoverLimits0, ordsets:new()),
 
@@ -437,6 +439,14 @@ create_shop_(
         {contract_modification, #payproc_ContractModificationUnit{
             id = ContractID,
             modification = {creation, ContractParams}
+        }},
+        {contract_modification, #payproc_ContractModificationUnit{
+            id = ContractID,
+            modification =
+                {payout_tool_modification, #payproc_PayoutToolModificationUnit{
+                    payout_tool_id = PayoutToolID,
+                    modification = {creation, PayoutToolParams}
+                }}
         }},
         ?shop_modification(ShopID, {creation, ShopParams}),
         ?shop_modification(ShopID, {shop_account_creation, ShopAccountParams}),
@@ -460,12 +470,13 @@ create_party_and_shop(PartyID, Category, Currency, TemplateRef, PaymentInstRef, 
     _ = create_party(PartyID, Client),
     create_shop(PartyID, Category, Currency, TemplateRef, PaymentInstRef, Client).
 
-make_shop_params(Category, ContractID) ->
+make_shop_params(Category, ContractID, PayoutToolID) ->
     #payproc_ShopParams{
         category = Category,
         location = {url, <<>>},
         details = #domain_ShopDetails{name = <<"Battle Ready Shop">>},
-        contract_id = ContractID
+        contract_id = ContractID,
+        payout_tool_id = PayoutToolID
     }.
 
 make_party_params() ->
@@ -547,6 +558,19 @@ make_contractor() ->
             representative_document = <<"100$ banknote">>,
             russian_bank_account = BankAccount
         }}}.
+
+-spec make_payout_tool_params() -> dmsl_payproc_thrift:'PayoutToolParams'().
+make_payout_tool_params() ->
+    #payproc_PayoutToolParams{
+        currency = ?cur(<<"RUB">>),
+        tool_info =
+            {russian_bank_account, #domain_RussianBankAccount{
+                account = <<"4276300010908312893">>,
+                bank_name = <<"SomeBank">>,
+                bank_post_account = <<"123129876">>,
+                bank_bik = <<"66642666">>
+            }}
+    }.
 
 -spec make_invoice_params(party_id(), shop_id(), binary(), cash()) -> invoice_params().
 make_invoice_params(PartyID, ShopID, Product, Cost) ->
